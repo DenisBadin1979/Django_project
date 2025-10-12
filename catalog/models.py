5669f5cd4d1c186f576c9a9ebaf2a41142e00611
Denis_Badin <denisbadin@yandex.ru>
2025-10-12 21:10:27 +0500

    Выполнено 2 задания , до миграции

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Продукт",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание продукта", help_text="Укажите описание продукта"
+    )
+    image = models.ImageField(upload_to="catalog/photo", blank=True, null=True)
+    category = models.ForeignKey(
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Введите категорию продукта",
+        blank=True,
+        null=True,
+        related_name="products",
+    )
+    purchase_price = models.DecimalField(
+        max_length=5, decimal_places=2, verbose_name="Цена продукта"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Продукты"
+        verbose_name_plural = "Продукты"
+        ordering = ["category", "name", "purchase_price"]
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Категория",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание категории",
+        help_text="Укажите описание категории",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
