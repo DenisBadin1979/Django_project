8db3cb613e7ffe8b9d8d124e9f7b15df75fec0e8
Denis_Badin <denisbadin@yandex.ru>
2025-10-13 09:38:00 +0500

    Выполнено 2 задания , после миграции 2.0

--- catalog/models.py
+++ catalog/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to="catalog/photo", blank=True, null=True)
     category = models.ForeignKey(
         "Category",
-        on_delete=models.SET_NULL,
+        on_delete=models.CASCADE,
         verbose_name="Категория",
         help_text="Введите категорию продукта",
         blank=True,
