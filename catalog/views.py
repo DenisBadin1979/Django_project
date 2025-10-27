4e9699a21f8f464fba87e9fb6b3fa5905e293979
Denis_Badin <denisbadin@yandex.ru>
2025-10-27 23:25:43 +0500

    Задание 1 перевели с FBV yf  CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,33 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-
+from django.views.generic import ListView, DetailView, TemplateView
 from catalog.models import Product
 
 
 def home(request):
     return render(request, "base.html")
 
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
 
-def contacts(request):
-    return render(request, "contacts.html")
+
+# def contacts(request):
+#     return render(request, "contacts.html")
 
 
-def contact_post(request):
-    if request.method == "POST":
-        # Получение данных из формы
-        name = request.POST.get("name")
-        message = request.POST.get("message")
-        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
-        # Здесь мы просто возвращаем простой ответ
-        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, "contacts.html")
 
-def product_list(request):
-    prod_list = Product.objects.all()
-    context = {"prod" : prod_list}
-    return render(request, "product_list.html", context)
 
-def product_detail(request, pk):
-    prod_detail = get_object_or_404(Product, pk=pk)
-    context = {"produ" : prod_detail}
-    return render (request, "prod_detail.html", context)
+
+class ProductListView(ListView):
+    model = Product
+
+class ProductDetailView(DetailView):
+    model = Product
+
+
