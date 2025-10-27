9ab60db8975e98010b02cd82d85100a612cc4266
Denis_Badin <denisbadin@yandex.ru>
2025-10-27 23:25:43 +0500

    Создано приложение blog и зарегестрировано в файле settings.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contact_post, contacts, home, product_list, product_detail
+from catalog.views import home, ProductListView, ProductDetailView, ContactsView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("base/", home, name="base"),
-    path("contacts/", contacts, name="contacts"),
-    path("contact_post/", contact_post, name="contact_post"),
-    path('', product_list, name="product_list"),
-    path('product/<int:pk>/', product_detail, name="product_detail"),
+    path("contacts/", ContactsView.as_view(), name="contacts"),
+    path('', ProductListView.as_view(), name="product_list"),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name="product_detail"),
 ]
