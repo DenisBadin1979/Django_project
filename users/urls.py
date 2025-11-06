35d01da8d7cf8a2d9dca81ff75d80be5f935a131
Denis_Badin <denisbadin@yandex.ru>
2025-11-07 03:02:04 +0500

    Home_27 пока что то с url надо исправлять

--- users/urls.py
+++ users/urls.py
@@ -13,7 +13,5 @@ urlpatterns = [
 
     path('logout/', LogoutView.as_view(next_page='catalog:home'), name='logout'),
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path("register/", RegisterView.as_view(), name="register"),
-
-
+    path("register/", RegisterView.as_view(template_name='users/register.html'), name="register"),
     ]
--- users/views.py
+++ users/views.py
@@ -5,4 +5,4 @@ from .forms import CustomUserCreationForm
 class RegisterView(CreateView):
     template_name = 'register.html'
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('catalog:home')
+    success_url = reverse_lazy('catalog/home')
