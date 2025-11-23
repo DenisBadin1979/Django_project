885a1d717d95228009e85e888929fabc0c56a981
Denis_Badin <denisbadin@yandex.ru>
2025-11-23 23:55:47 +0500

    settings.py 27

--- config/settings.py
+++ config/settings.py
@@ -111,8 +111,8 @@ EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'sudjango@yandex.ru'
-EMAIL_HOST_PASSWORD = 'hibidvkvecapboju'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGIN_REDIRECT_URL = "catalog:product_list"
